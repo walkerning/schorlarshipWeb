62a2203fdb4e47c336e98828ba0876d84411df64
unknown <>
2017-06-02 15:35:29 +0800

    add /api/v1/honors /api/v1/honor/{:id}

---
+++ src/api/honors.js
@@ -0,0 +1,29 @@
+var _ = require("lodash")
+var Promise = require("bluebird")
+var util = require("util")
+var models = require("../models")
+var errors = require("../errors")
+var logging = require("../logging")
+
+module.exports={
+  list:function list(req, res, next){
+    var queries=req.query;
+    return models.Honors.getByQuery(queries,["groups"])
+      .then(function(collection){
+        res.json(collection.toClientJSON());
+        return null;
+      })
+  },
+
+  info: function info(req, res, next){
+    return models.Honor
+      .getById(req.params.honorId,{fetchOptions:{withRelated:['groups']}})
+        .then(function(honor){
+          res.status(200).json(honor.toClientJSON());
+      })
+
+  },
+
+
+
+}
--- src/models/honors.js
+++ src/models/honors.js
@@ -1,10 +1,23 @@
 var bookshelfInst = require("./base");
+var _ = require("lodash")
+var Quota = bookshelfInst.Model.extend({
+  tableName: "groups_honors",
+
+  groups: function() {
+    return this.belongsTo("Group");
+  },
+
+  honors: function() {
+    return this.belongsTo("Honor");
+  }
+});
+
 
 var Honor = bookshelfInst.Model.extend({
   tableName: "honors",
 
   groups: function() {
-    return this.beglongToMany("Group");
+    return this.belongsToMany("Group").withPivot(["quota"]);
   },
 
   users: function() {
@@ -13,14 +26,82 @@ var Honor = bookshelfInst.Model.extend({
 
   form: function() {
     return this.belongsTo("Form", "form_id");
+  },
+
+
+
+  omitAttributes: function omitAttributes(){
+    return ["created_at",
+      "created_by",
+      "updated_at",
+      "updated_by",
+      "groups"
+    ];
+  },
+
+  renamePivotAttributes: function renamePivotAttributes(){
+    return {
+      _pivot_quota:"quota",
+      description:"type",
+      name:"group"
+    };
+  },
+
+  pickPivotAttributes: function pickPivotAttributes(){
+    return ["group",
+      "quota",
+      "type"
+    ];
+  },
+
+  toClientJSON: function toClientJSON(options){
+    var quota=0;
+
+    var json=this.toJSON();
+    json["group_quota"]=json["groups"];
+
+    var renamePivotAttributes=this.renamePivotAttributes();
+    var pickPivotAttributes=this.pickPivotAttributes();
+    json["group_quota"]=_.map(json["group_quota"],function(model){
+      _.forEach(renamePivotAttributes,function(value,key){
+        model[value]=model[key];
+      });
+      model=_.pick(model,pickPivotAttributes);
+      quota+=model["quota"];
+      return model;
+    });
+    json["quota"]=quota;
+    return _.omit(json,this.omitAttributes());
   }
 });
 
+
 var Honors = bookshelfInst.Collection.extend({
   model: Honor
+}, {
+  queriableAttributes: function queriableAttributes(){
+    return ["id",
+      "name",
+      "year",
+      "start_time",
+      "end_time",
+      "form_id"
+    ];
+  },
+  getByQuery: function(query,related)
+  {
+    return this.forge()
+      .query({
+        where: _.pick(query, this.queriableAttributes())
+      })
+      .fetch({withRelated:related});
+  }
+
 });
 
 module.exports = {
   Honor: bookshelfInst.model("Honor", Honor),
-  Honors: bookshelfInst.collection("Honors", Honors)
+  Honors: bookshelfInst.collection("Honors", Honors),
+  Quota: bookshelfInst.model("Quota", Quota),
+  Quotas: bookshelfInst.collection("Quotas", Honors)
 };
